3aa39467f5ad377fec71843c8a221b52316ddd5b
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-26 13:22:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName(x) {
+  var b = ''
+  if (x = 1) {
+    b = (customerName = 'BOB')
+  }
+  return b
+}
+
+function setBestCustomer(value) {
+  bestCustomer = ''
+  if (value = 1){
+    bestCustomer= 'not bob'
+  }
+  return bestCustomer
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = ''
+  if (){
+    bestCustomer = 'maybe bob'
+  }
+  return bestCustomer
+
+}
