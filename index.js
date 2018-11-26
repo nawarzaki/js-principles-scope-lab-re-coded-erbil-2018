b723ef66e07cdb78f49dee915b17406ed3aa4968
nawaar.zakee <nawaar.zakee@yahoo.com>
2018-11-26 13:40:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'adam'
+function upperCaseCustomerName(x) {
+  var b = ''
+  if (x = 1) {
+    b = (customerName = 'BOB')
+  }
+  return b
+}
+
+function setBestCustomer(value) {
+  bestCustomer = ''
+  if (value = 1){
+    bestCustomer= 'not bob'
+  }
+  return bestCustomer
+}
+
+function overwriteBestCustomer(value) {
+  bestCustomer = setBestCustomer(value)
+  if (bestCustomer = 'not bob'){
+    bestCustomer = 'maybe bob'
+  }
+  return bestCustomer
+}
+
+function changeLeastFavoriteCustomer(x){
+let b = ''
+if (x = 1) {
+  b = (leastFavoriteCustomer = 'adam')
+}
+return b
+}
